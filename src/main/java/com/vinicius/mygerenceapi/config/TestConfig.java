9a5228b4f7a2d59173122f0651acb541a2decc11
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2022-07-24 20:48:00 -0300

    Perfil de teste

---
+++ src/main/java/com/vinicius/mygerenceapi/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.vinicius.mygerenceapi.config;
+
+import com.vinicius.mygerenceapi.service.DBServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBServices dbServices;
+
+    @Bean
+    public void instanciaBD(){
+        this.dbServices.instanciaBD();
+    }
+}
---
+++ src/main/java/com/vinicius/mygerenceapi/service/DBServices.java
@@ -0,0 +1,30 @@
+package com.vinicius.mygerenceapi.service;
+
+import com.vinicius.mygerenceapi.model.entity.Categoria;
+import com.vinicius.mygerenceapi.model.entity.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.vinicius.mygerenceapi.repositories.CategoriaRepository;
+import com.vinicius.mygerenceapi.repositories.ProdutoRepository;
+
+import java.util.Arrays;
+
+@Service
+public class DBServices {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public void instanciaBD(){
+        Categoria cat1 = new Categoria(null, "Hardware", "Produtos fisicos");
+        Produto p1 = new Produto(null, "Mouse","Hoopson","Mouse USB",cat1);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1));
+
+
+        //Salvando todos os dados no banco de dados através do JPA Repository
+        this.categoriaRepository.saveAll(Arrays.asList(cat1));
+        this.produtoRepository.saveAll(Arrays.asList(p1));
+    }
+}
