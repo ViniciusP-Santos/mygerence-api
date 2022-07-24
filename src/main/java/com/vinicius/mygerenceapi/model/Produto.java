c17b09e83872784de184a1047825dfafc2df378c
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2022-07-24 19:15:53 -0300

    Classes model iniciadas

---
+++ src/main/java/com/vinicius/mygerenceapi/model/Produto.java
@@ -0,0 +1,77 @@
+package com.vinicius.mygerenceapi.model;
+
+import java.util.Objects;
+
+public class Produto {
+    private Integer id;
+    private String nome;
+    private String marca;
+    private String conteudo;
+
+    private Categoria categoria;
+
+    public Produto(){
+        super();
+    }
+
+    public Produto(Integer id, String nome, String marca, String conteudo, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+        this.conteudo = conteudo;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(getId(), produto.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
