ec0ef2ce18088cac420b4da70bf5620d74e4bf85
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2022-07-24 19:01:49 -0300

    Inicializando o App

---
+++ src/main/java/com/vinicius/mygerenceapi/MygerenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.mygerenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygerenceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MygerenceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinicius/mygerenceapi/MygerenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.mygerenceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygerenceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
