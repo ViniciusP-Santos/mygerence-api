4b5377338ac7331c54d346e7b7504c7532758450
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2022-07-24 21:47:33 -0300

    Excessão Personalizada

--- src/main/java/com/vinicius/mygerenceapi/model/service/CategoriaServices.java
+++ src/main/java/com/vinicius/mygerenceapi/model/service/CategoriaServices.java
@@ -1,6 +1,7 @@
 package com.vinicius.mygerenceapi.model.service;
 
 import com.vinicius.mygerenceapi.model.entity.Categoria;
+import com.vinicius.mygerenceapi.model.service.exception.ObjectNotFoundException;
 import com.vinicius.mygerenceapi.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,8 @@ public class CategoriaServices {
 
     public Categoria findById(Integer id){
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+
+        //Tratando a excessão, caso a categoria não exista
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: "+ id + ", Tipo: "+Categoria.class.getName()));
     }
 }
