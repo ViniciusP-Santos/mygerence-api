71741c341c0e4bf33802efdf9bf0f5cc372c2dfe
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2022-07-31 14:50:06 -0300

    Adicionando context de autorização spring

---
+++ src/main/java/com/vinicius/mygerenceapi/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.vinicius.mygerenceapi.security.services;
+
+import com.vinicius.mygerenceapi.model.entity.User;
+import com.vinicius.mygerenceapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com nome de usuário: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+
+}
