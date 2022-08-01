dbeff12d7064913f9d31489543572692130ff157
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2022-07-31 21:07:01 -0300

    Resolvendo bugs de login

--- src/main/java/com/vinicius/mygerenceapi/controller/AuthController.java
+++ src/main/java/com/vinicius/mygerenceapi/controller/AuthController.java
@@ -34,6 +34,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
